91652120deb1413e58a156e88b1a716880781c22
Bless-Khing <kwame.bee001@gmail.com>
2024-12-20 03:23:31 +0100

    script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const apiUrl = 'http://localhost:3000/api/weather/';
-
+// Set the base URL for the API depending on the environment
+const apiUrl = 'https://bless-weather-app.vercel.app/api/weather/'; // Vercel deployment URL for production
+// const apiUrl = 'http://localhost:3000/api/weather/'; // Uncomment this line for local development
 
 function getWeather() {
-    const location = document.getElementById('locationInput').value;
-    
-    fetch(`${apiUrl}${location}`)
+    const location = document.getElementById('locationInput').value; // Get user input
+    fetch(`${apiUrl}${location}`) // Construct the full URL by appending the city name
         .then(response => {
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
-            return response.json();
+            return response.json(); // Parse JSON response
         })
         .then(data => {
-            displayWeather(data); // Handle displaying weather data
+            displayWeather(data); // Call function to display weather data on the UI
         })
         .catch(error => {
             console.error('Error fetching weather data:', error);
